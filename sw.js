5843d608f88807d93f59c211d8359c11fa9a4b2e
yoyo196204-hub <yoyo196204@gmail.com>
2026-03-07 09:55:05 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-
-const CACHE_NAME="forge-v2-cache";
-const urlsToCache=["./","./index.html"];
-self.addEventListener("install",e=>{
- e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)));
-});
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
